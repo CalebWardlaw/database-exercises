35972eca97f5771bb7c3d6fea4654214b0a6f8ac
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-01-14 09:49:25 -0600

    populated albums table with seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,16 +1,13 @@
 USE codeup_test_db;
 
-DROP TABLE IF  EXISTS albums;
+DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) NOT NULL DEFAULT 'Unknown',
-    name VARCHAR(50) NOT NULL DEFAULT 'Untitled',
-    release_date INT UNSIGNED,
-    sales FLOAT UNSIGNED,
-    genre VARCHAR(100),
-    PRIMARY KEY(id)
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       artist VARCHAR(255) NOT NULL DEFAULT 'Unknown',
+                       name VARCHAR(255) NOT NULL DEFAULT 'Untitled' UNIQUE,
+                       release_date INT UNSIGNED,
+                       sales FLOAT UNSIGNED,
+                       genre VARCHAR(100),
+                       PRIMARY KEY(id)
 );
-
-INSERT INTO albums (artist, name)
-VALUES ('Funkadelic', 'Music for My Mother');
