7d5d5f567b5d9d85a975ab36aafdb5de76f2dd62
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-01-17 17:36:01 -0600

    comlpleted group_by_exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,3 +12,21 @@ SELECT last_name, first_name FROM employees
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%e'
 GROUP BY last_name, first_name;
+
+SELECT last_name FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%'
+   OR last_name LIKE 'E%e'
+GROUP BY last_name
+ORDER BY last_name;
+
+SELECT COUNT(gender), gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
